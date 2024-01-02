6b2622ca7fe2d484fd309efecb95b253d7675322
Nikita Akimov <korchiy@yandex.ru>
2024-01-02 11:48:26 +0300

    update

--- na_1d_tools_ui.py
+++ na_1d_tools_ui.py
@@ -5,7 +5,8 @@
 #   https://github.com/Korchy/1d_na_tools
 
 
-from bpy.types import Panel
+from bpy.props import BoolProperty
+from bpy.types import Panel, Scene
 from bpy.utils import register_class, unregister_class
 from .we_crease_from_seam.we_crease_from_seam import WECFS
 
@@ -18,12 +19,55 @@ class NA_1D_TOOLS_PT_panel(Panel):
 	bl_category = 'NA 1D TOOLS'
 
 	def draw(self, context):
-		WECFS.ui(layout=self.layout)
+		layout = self.layout
+		# CONVERT
+		self.ui_section(
+			layout=layout,
+			context=context,
+			prop='na_1d_tools_opts_ui_convert_section',
+			label='CONVERT'
+		)
+		if context.scene.na_1d_tools_opts_ui_convert_section:
+			# we crease from seam
+			WECFS.ui(layout=layout)
+		# Material 1D Select
+		
+
+		# EDIT TOOLS
+		self.ui_section(
+			layout=layout,
+			context=context,
+			prop='na_1d_tools_opts_ui_edit_section',
+			label='EDIT TOOLS'
+		)
+		if context.scene.na_1d_tools_opts_ui_edit_section:
+			# we crease from seam
+			# WECFS.ui(layout=layout)
+			pass
+
+	@staticmethod
+	def ui_section(layout, context, prop, label):
+		row = layout.row()
+		icon = 'TRIA_DOWN' if getattr(context.scene, prop) else 'TRIA_RIGHT'
+		row.prop(
+			context.scene,
+			prop,
+			icon=icon,
+			text=label
+		)
 
 
 def register():
+	Scene.na_1d_tools_opts_ui_convert_section = BoolProperty(
+		default=False
+	)
+	Scene.na_1d_tools_opts_ui_edit_section = BoolProperty(
+		default=False
+	)
 	register_class(NA_1D_TOOLS_PT_panel)
 
 
 def unregister():
 	unregister_class(NA_1D_TOOLS_PT_panel)
+	del Scene.na_1d_tools_opts_ui_edit_section
+	del Scene.na_1d_tools_opts_ui_convert_section
